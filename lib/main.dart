3bdf48f64f591f24c0ab85d64987e2ad3a3d48e3
Steffane Ribeiro <Ste-ufrpe@PPM-SPO-12237.local>
2021-04-22 15:39:16 -0300

    Atividade - Semana 3 (clone da branch_wordPairs1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,152 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+Map<WordPair, bool> wordPairs;
 
+void main() {
+  initWordPairs();
+  runApp(MyApp());
+}
+
+void initWordPairs() {
+  final generatedWordPairs = generateWordPairs().take(20);
+  wordPairs =
+      Map.fromIterable(generatedWordPairs, key: (e) => e, value: (e) => null);
+}
+
+///App baseado no tutorial do Flutter disponível em:
+///https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'App de Listagem - DSI/BSI/UFRPE',
       theme: ThemeData(
-        primaryColor: Colors.deepPurple,
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: RandomWords(),
+      home: HomePage(),
     );
   }
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = TextStyle(fontSize: 18.0);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
+class _HomePageState extends State<HomePage> {
+  int pageIndex = 0;
+  List<Widget> _pages = [
+    RandomWordsListPage(null),
+    RandomWordsListPage(true),
+    RandomWordsListPage(false)
+  ];
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.deepPurple : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
+  void _changePage(int value) {
+    setState(() {
+      pageIndex = value;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: [
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
+        title: Text('App de Listagem - DSI/BSI/UFRPE'),
       ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-                (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
+      body: _pages[pageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _changePage,
+        currentIndex: pageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.thumb_up_outlined),
+            label: 'Liked',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.thumb_down_outlined),
+            label: 'Disliked',
+          ),
+        ],
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class RandomWordsListPage extends StatefulWidget {
+  final bool _filter;
+  RandomWordsListPage(this._filter);
+
   @override
-  State<RandomWords> createState() => _RandomWordsState();
+  _RandomWordsListPageState createState() => _RandomWordsListPageState();
+}
+
+///Esta classe é o estado da classe que lista os pares de palavra
+///
+class _RandomWordsListPageState extends State<RandomWordsListPage> {
+  final _icons = {
+    null: Icon(Icons.thumbs_up_down_outlined),
+    true: Icon(Icons.thumb_up, color: Colors.blue),
+    false: Icon(Icons.thumb_down, color: Colors.red),
+  };
+
+  Iterable<WordPair> get items {
+    if (widget._filter == null) {
+      return wordPairs.keys;
+    } else {
+      //a linah aabaixo retorna os pares filtras opelo filtro
+      return wordPairs.entries
+          .where((element) => element.value == widget._filter)
+          .map((e) => e.key);
+    }
+  }
+
+  _toggle(WordPair wordPair) {
+    bool like = wordPairs[wordPair];
+    if (widget._filter != null) {
+      wordPairs[wordPair] = null;
+    } else if (like == null) {
+      wordPairs[wordPair] = true;
+    } else if (like == true) {
+      wordPairs[wordPair] = false;
+    } else {
+      wordPairs[wordPair] = null;
+    }
+    setState(() {});
+  }
+
+  String capitalize(String s) {
+    return '${s[0].toUpperCase()}${s.substring(1)}';
+  }
+
+  String asString(WordPair wordPair) {
+    return '${capitalize(wordPair.first)} ${capitalize(wordPair.second)}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemCount: items.length * 2,
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final int index = i ~/ 2;
+          return _buildRow(index + 1, items.elementAt(index));
+        });
+  }
+
+  Widget _buildRow(int index, WordPair wordPair) {
+    return ListTile(
+      title: Text('$index. ${asString(wordPair)}'),
+      trailing: _icons[wordPairs[wordPair]],
+      onTap: () => _toggle(wordPair),
+    );
+  }
 }
