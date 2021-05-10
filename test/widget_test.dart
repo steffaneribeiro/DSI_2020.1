0ec3f9d528bbb98c7f8c26fdd5312069fad3c0c6
Steffane Ribeiro <Ste-ufrpe@PPM-SPO-12237.local>
2021-05-10 06:28:30 -0300

    Atividade - Semana 3 (clone da branch_wordPairs3 e edição)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:atividades_dsi/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(DSIApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
